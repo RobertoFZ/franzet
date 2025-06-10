4a78547f2dac3e923defc7cdc5dd51dca40819e2
Roberto Franco <roberto.zetinca@outlook.com>
2025-06-10 15:13:59 -0600

    Feat: add speed insights feature from nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const poppins = Poppins({
@@ -116,6 +117,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} font-sans`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
